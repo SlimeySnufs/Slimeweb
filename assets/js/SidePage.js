79c09d4d12e74c1835a0e5a18c29f53f51b7b375
skibbslime <81821533+SlimeySnufs@users.noreply.github.com>
2025-08-18 22:43:40 -0400

    Custom Cursors Implemented

--- assets/js/SidePage.js
+++ assets/js/SidePage.js
@@ -3,6 +3,5 @@ const mainPageButton = document.getElementById("MainPageButton");
 mainPageButton.addEventListener("click", () => {
     // Logic to navigate to the main page
     window.location.href = "mainpage.html";
-    window.History.pushState(null, "", window.location.href);
+    window.history.pushState(null, "", window.location.href);
 });
-    
