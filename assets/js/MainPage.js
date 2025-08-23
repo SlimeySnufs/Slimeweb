892400bf113ac7f394d8079239274c4873e77154
skibbslime <225761818+skibtranslator@users.noreply.github.com>
2025-08-22 21:11:30 -0400

    Login Page Functional minus DB interaction

--- assets/js/MainPage.js
+++ assets/js/MainPage.js
@@ -1,6 +1,12 @@
 const BackButton = document.getElementById("BackButton");
+const ProfileButton = document.getElementById("ProfileButton");
 
 BackButton.addEventListener("click", () => {
     window.history.back();
+    window.history.pushState(null, "", window.location.href);
+});
+
+ProfileButton.addEventListener("click", () => {
+    window.location.href = "loginpage.html";
     window.history.pushState(null, "", window.location.href);
 });
